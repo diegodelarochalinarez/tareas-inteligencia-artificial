10deeab86a51059b0aba51eb1919416dbab985c2
MixUp <mixup@localhost.localdomain>
2025-01-31 11:52:53 -0700

    Creación del árbol y de los nodos para implementar, falta metodo de agregar y busqueda :P

---
+++ Node.py
@@ -0,0 +1,31 @@
+class Node:
+    
+    def __init__(self, valor, right = None, left = None, parent = None ):
+        self.valor= valor
+        self.right = right
+        self.left = left
+        self.parent = parent
+    
+    def hasRight(self):
+        return self.right
+    
+    def hasLeft(self):
+        return self.left
+    
+    def isRoot(self):
+        return not self.parent
+    
+    def isLeftSon(self):
+        return self.parent and self.parent.left == self
+    
+    def isRightSon(self):
+        return self.parent and self.parent.right == self
+    
+    def isLeaf(self):
+        return not (self.right or self.left)
+    
+    def hasChild(self):
+        return self.right or self.left
+    
+    def hasBothChildren(self):
+        return self.right and self.left
